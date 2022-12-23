15452c29853092832dd041aded3ccd80054fe009
Palash <palashnath880@gmail.com>
2022-12-23 18:25:58 +0600

    Slider Updated

--- src/pages/Home/Slider/Slider.js
+++ src/pages/Home/Slider/Slider.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import sliderImg from '../../../images/slider1.png';
+import slider1 from '../../../images/slider/slider1.png';
+import slider2 from '../../../images/slider/slider2.png';
+import slider3 from '../../../images/slider/slider3.png';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from 'swiper';
 import SlideItem from '../../../components/SlideItem/SlideItem';
@@ -22,13 +24,28 @@ const Slider = () => {
                     loop={true}
                 >
                     <SwiperSlide>
-                        <SlideItem sliderImg={sliderImg} />
+                        <SlideItem
+                            sliderImg={slider1}
+                            team1='India'
+                            team2='Bangladesh'
+                            match='Indian Premier League 2022'
+                        />
                     </SwiperSlide>
                     <SwiperSlide>
-                        <SlideItem sliderImg={sliderImg} />
+                        <SlideItem
+                            sliderImg={slider2}
+                            team1='India'
+                            team2='Bangladesh'
+                            match='Indian Premier League 2022'
+                        />
                     </SwiperSlide>
                     <SwiperSlide>
-                        <SlideItem sliderImg={sliderImg} />
+                        <SlideItem
+                            sliderImg={slider3}
+                            team1='India'
+                            team2='Bangladesh'
+                            match='Indian Premier League 2022'
+                        />
                     </SwiperSlide>
                 </Swiper>
             </div>
