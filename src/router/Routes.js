07248e9128f6454da94ae350f7eb28335470bfc9
Nishant Sharma <nishant-ai@outlook.com>
2022-12-25 11:50:03 +0530

    Merge Fixes

--- src/router/Routes.js
+++ src/router/Routes.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import MainLayout from '../layouts/Main/MainLayout';
-import Home from '../pages/Home/Home';
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainLayout from "../layouts/Main/MainLayout";
+import Home from "../pages/Home/Home";
 
 const Routes = () => {
   const router = createBrowserRouter([
@@ -10,20 +10,14 @@ const Routes = () => {
       element: <MainLayout />,
       children: [
         {
-            path: '/',
-            element: <MainLayout />,
-            children: [
-                {
-                    path: '/',
-                    element: <Home />
-                }
-            ]
-        }
-    ]);
-    return (
-        <RouterProvider router={router}>
-        </RouterProvider>
-    );
-}
+          path: "/",
+          element: <Home />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router}></RouterProvider>;
+};
 
 export default Routes;
