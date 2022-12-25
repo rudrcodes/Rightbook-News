b59223bd3ca19b8ee9bfb3146a9c71baea3db3e6
Nishant Sharma <nishant-ai@outlook.com>
2022-12-25 11:53:04 +0530

    Merge Fixes

--- src/router/Routes.js
+++ src/router/Routes.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MainLayout from "../layouts/Main/MainLayout";
+import Cricket from "../pages/Cricket/Cricket";
+import CricketMatches from "../pages/Cricket/CricketMatches/CricketMatches";
+import Football from "../pages/Football/Football";
+import FootballMatches from "../pages/Football/FootballMatches/FootballMatches";
+import FootballSchedules from "../pages/Football/FootballSchedules/FootballSchedules";
 import Home from "../pages/Home/Home";
 
 const Routes = () => {
@@ -13,10 +18,33 @@ const Routes = () => {
           path: "/",
           element: <Home />,
         },
+        {
+          path: "/cricket",
+          element: <Cricket />,
+          children: [
+            {
+              path: "/cricket/",
+              element: <CricketMatches />,
+            },
+          ],
+        },
+        {
+          path: "/football",
+          element: <Football />,
+          children: [
+            {
+              path: "/football",
+              element: <FootballMatches />,
+            },
+            {
+              path: "/football/schedules",
+              element: <FootballSchedules />,
+            },
+          ],
+        },
       ],
     },
   ]);
-
   return <RouterProvider router={router}></RouterProvider>;
 };
 
